6438ec3248d3bed4ad1772bcbf6a2a821b17c06c
tgszwdv <102832569+tgszwdv@users.noreply.github.com>
2024-08-15 20:29:18 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -5,20 +5,23 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
+// Inicializa o aplicativo Express
 const app = express();
 
 const mongoURI = process.env.MONGO_URI;
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 // Configuração do middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/pages')));
 
+// Conectar ao MongoDB Atlas
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado ao MongoDB Atlas com sucesso'))
   .catch(err => console.error('Erro de conexão com o MongoDB:', err));
 
+// Definir o modelo Mongoose para Processos
 const processoSchema = new mongoose.Schema({
   titulo: String,
   descricao: String,
@@ -29,13 +32,23 @@ const processoSchema = new mongoose.Schema({
 
 const Processo = mongoose.model('Processo', processoSchema);
 
+// Rota para iniciar o scraping e atualizar o banco de dados
 app.get('/scrape', async (req, res) => {
+  let browser;
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: process.env.CHROME_EXECUTABLE_PATH || puppeteer.executablePath()
+    browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
+
     const page = await browser.newPage();
     const url = 'https://selecao-login.app.ufgd.edu.br/';
     
@@ -67,8 +80,6 @@ app.get('/scrape', async (req, res) => {
       return processos;
     });
 
-    await browser.close();
-
     await Processo.deleteMany({});
     await Processo.insertMany(processos);
 
@@ -76,9 +87,14 @@ app.get('/scrape', async (req, res) => {
   } catch (error) {
     console.error('Erro ao acessar a página de scraping:', error.message);
     res.status(500).send('Erro ao acessar a página de scraping');
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 });
 
+// Rota para atualizar os dados no banco de dados
 app.post('/atualizar', async (req, res) => {
   try {
     const { processosAtualizados } = req.body;
@@ -97,6 +113,7 @@ app.post('/atualizar', async (req, res) => {
   }
 });
 
+// Rota para obter processos abertos
 app.get('/processosAbertos', async (req, res) => {
   try {
     const processos = await Processo.find({});
@@ -107,10 +124,12 @@ app.get('/processosAbertos', async (req, res) => {
   }
 });
 
+// Rota para servir o arquivo index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/pages', 'index.html'));
 });
 
+// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
